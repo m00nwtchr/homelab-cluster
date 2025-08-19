96cc30f77db2f66e2e827e2abb5de1a0956eb5a0
m00nwtchr <m00nwtchr@duck.com>
2025-08-19 20:13:58 +0200

    k8s/kube-guardian: add Flux Kustomization, HelmRelease, and devShell integration

--- flake.nix
+++ flake.nix
@@ -1,43 +1,73 @@
 {
-	description = "DevShell with Kubernetes tools";
+  description = "DevShell with Kubernetes tools";
 
-	inputs = {
-#		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-		flake-utils.url = "github:numtide/flake-utils";
-	};
+  inputs = {
+    #		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-	outputs = { self, nixpkgs, flake-utils, ... }:
-		flake-utils.lib.eachDefaultSystem (system:
-			let
-				pkgs = import nixpkgs {
-					inherit system;
-				};
-			in {
-				devShells.default = pkgs.mkShell {
-					name = "k8s-devshell";
-					packages = with pkgs; [
-						kubectl
-						kubelogin-oidc
-						talosctl
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
 
-            (wrapHelm kubernetes-helm {
-              plugins = with pkgs.kubernetes-helmPlugins; [
-                helm-diff
-              ];
-            })
-            helmfile
-						kubeseal
+      xentra = pkgs.stdenv.mkDerivation rec {
+        name = "kubectl-xentra-bin-${version}";
+        version = "0.0.19";
 
-						cilium-cli
-            fluxcd
+        src = pkgs.fetchurl {
+          url = "https://github.com/xentra-ai/kube-guardian/releases/download/v${version}/advisor-linux-amd64";
+          sha256 = "sha256-FC9N7uS2xFEIiuB3OAELSueafYK7uS10chBhm92RO1c=";
+        };
 
-            go-task
-            sops
+        dontUnpack = true;
 
-						jq    # JSON parser (handy for `kubectl` output)
-						yq
-						gh    # GitHub CLI (for auth and repo ops)
-					];
-				};
-			});
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ${src} $out/bin/kubectl-xentra
+          chmod +x $out/bin/kubectl-xentra
+        '';
+
+        meta = with pkgs.lib; {
+          description = "A Kubernetes tool leveraging eBPF for advanced Kubernetes security, auto-generating Network Policies, Seccomp Profiles, and more.";
+          homepage = "https://github.com/xentra-ai/kube-guardian";
+          license = licenses.asl20;
+          platforms = platforms.linux;
+        };
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "k8s-devshell";
+        packages = with pkgs; [
+          kubectl
+          kubelogin-oidc
+          xentra
+          talosctl
+
+          (wrapHelm kubernetes-helm {
+            plugins = with pkgs.kubernetes-helmPlugins; [
+              helm-diff
+            ];
+          })
+          helmfile
+          kubeseal
+
+          cilium-cli
+          fluxcd
+
+          go-task
+          sops
+
+          jq # JSON parser (handy for `kubectl` output)
+          yq
+          gh # GitHub CLI (for auth and repo ops)
+        ];
+      };
+    });
 }
