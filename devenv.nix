e162a19fd6f8bafb0d0f0f75735abe8fbf066e40
m00nwtchr <m00nwtchr@duck.com>
2026-01-08 11:23:12 +0100

    switch to devenv.sh

---
+++ devenv.nix
@@ -0,0 +1,52 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+{
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+
+    kubectl
+    kubelogin-oidc
+    talosctl
+
+    helmfile
+    kubeseal
+
+    cilium-cli
+    fluxcd
+
+    go-task
+    sops
+
+    jq # JSON parser (handy for `kubectl` output)
+    yq
+    gh # GitHub CLI (for auth and repo ops)
+  ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    helm = {
+      enable = true;
+      plugins = [ "helm-diff" ];
+    };
+    opentofu.enable = true;
+  };
+
+  treefmt = {
+    enable = true;
+    config.programs = {
+      nixfmt.enable = true;
+      yamllint.enable = true;
+    };
+  };
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    treefmt.enable = true;
+  };
+}
